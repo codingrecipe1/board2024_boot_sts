f93b16bf85f54a58aa756ee9583f6b46927e1f01
codingrecipe1 <codingrecipe1@gmail.com>
2024-05-30 18:01:52 +0900

    01_project setting

---
+++ src/main/java/com/codingrecipe/board/Board2024BootStsApplication.java
@@ -0,0 +1,13 @@
+package com.codingrecipe.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board2024BootStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board2024BootStsApplication.class, args);
+	}
+
+}
